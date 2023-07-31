8b4054782ad1be6119d35afc4154e4bb3a8c8921
Asad Ishfaq <masad@alec.ae>
2023-07-31 15:13:55 +0500

    remove styles

--- App.tsx
+++ App.tsx
@@ -8,8 +8,7 @@
 import type { PropsWithChildren } from 'react';
 import
   {
-    SafeAreaView,
-    StyleSheet
+    SafeAreaView
   } from 'react-native';
 
 
@@ -21,23 +20,5 @@ function App(): JSX.Element {
   return <SafeAreaView></SafeAreaView>;
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
