7654a788221b7901bff5176ad765679e2045ab6e
saad-mehmood <saad.mehmood@aquila360.com>
2023-07-31 16:27:56 +0500

    add sign up Screen

--- app/helpers/screenConstant.ts
+++ app/helpers/screenConstant.ts
@@ -1,3 +1,4 @@
 export const Screens = {
   login: 'LoginScreen',
+  signup: 'SignUpScreen',
 };
---
+++ app/screens/SignUpScreen.tsx
@@ -0,0 +1,12 @@
+import {Text} from 'native-base';
+import Layout from '../components/Layout';
+
+const SignUpScreen = () => {
+  return (
+    <Layout>
+      <Text>Signup Screen</Text>
+    </Layout>
+  );
+};
+
+export default SignUpScreen;
