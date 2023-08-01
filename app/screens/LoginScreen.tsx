9ab20d020b39e04e8ef0819dd601304d8ac0685d
iamsaadmehmood <warriorex.g@gmail.com>
2023-08-01 17:31:46 +0500

    add button component and add navigation to signup screen from login screen

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,10 +1,20 @@
 import {Text} from 'native-base';
 import Layout from '../components/Layout';
+import ButtonComponent from '../components/ButtonComponent';
+import {navigate} from '../services/navigation.service';
+import {Screens} from '../helpers/screenConstant';
 
 const LoginScreen = () => {
   return (
     <Layout>
       <Text>Login</Text>
+      <ButtonComponent
+        onPress={() => navigate(Screens.login)}
+        title={'Create an Account'}
+        marginTop={0}
+        type={'Primary'}
+        width={90}
+      />
     </Layout>
   );
 };
--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -1,11 +1,13 @@
 const Light = {
   background: '#FFFFFF',
   primary: '#212121',
+  primary500: '#3b5998',
 };
 
 const Dark = {
   background: '#000000',
   primary: '#FFFFFF',
+  primary500: '#3b5998',
 };
 
 export const Colors = Dark;
